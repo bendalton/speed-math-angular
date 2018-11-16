416b5875345859202aaca1f8ec99a2907cc5e9e7
ben dalton <bendalton@rideamigos.com>
2018-11-16 02:40:15 -0500

    Making the deploy script more fun

--- update-site.sh
+++ update-site.sh
@@ -1,4 +1,8 @@
+echo "🌎 Releasing to the world..."
 rm -rf dist/*
+echo "💻 Building App"
 ng build --prod --base-href="https://bendalton.github.io/speed-math-angular/"
 mv dist/CalebMath/* dist/.
+echo "📲 Publishing to Github"
 ngh
+echo "View the updated site soon (10 minutes?) here: https://bendalton.github.io/speed-math-angular/"
